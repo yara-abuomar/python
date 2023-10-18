ff7f401d4f19fe603d27c516f893e4504f4e3347
yara-abuomar <yaraabuomar@icloud.com>
2023-10-18 13:42:24 +0300

    hello world ass

---
+++ helloworld/hello_world.py
@@ -0,0 +1,12 @@
+print("Hello World")
+y="Yara"
+print("Hello",y)
+print("Hello "+y)
+x=1
+print("Hello",x)
+print("Hello "+str(x))
+p="Pizza"
+k="Kabab"
+print("I love to eat {} and {}." .format(p,k))
+print(f"I love to eat {p} and {k}.")
+
