425cc3f0f2248be422516a8855e33861c44e2e59
yara-abuomar <147878514+yara-abuomar@users.noreply.github.com>
2023-11-25 01:47:30 +0200

    Assignment: Semi-Restful TV Shows Validated

--- app1/views.py
+++ app1/views.py
@@ -1,41 +1,62 @@
-from django.shortcuts import render ,redirect
-import random	
+from django.shortcuts import render ,redirect 
+from .models import *
+from django.contrib import messages
 
-def guees(request):
-    request.session['num']=random.randint(1, 100)
-    print(request.session['num'])
+def method(request):
+    return render(request ,'form.html' )
+
+def method1 (request):
+    print("hiii")
+    print(request.POST['releasedate'])
+    errors=Show.objects.basic_validator(request.POST)
+    if len(errors)>0:
+        for key ,values in errors.items():
+            messages.error(request,values)
+        return redirect('/shows/new')
+
+    show1=Show.creatshow(title1=request.POST['tname'],network1=request.POST['net'],description1=request.POST['desc'],reldate=request.POST['releasedate'])
+    return redirect('/shows/'+ str(show1.id))
+def method2(request):
+    context={
+        'show':Show.readone()
+    }
+    return render(request ,'show.html' ,context)
+
+def method3(request):
+    context={
+        'all':Show.readshow(),
+    }
+    return render(request ,'all.html' ,context)
+def method4(request,id):
+    ss=Show.objects.get(id=int(id))
+    ss.delete()
+    return redirect('/shows')
+
+def method5(request,num):
+    context={
+         'show':Show.objects.get(id=int(num))
+     }
+    return render(request ,'show.html' ,context)
+
+def method6(request,num2):
+    errors1=Show.objects.basic_validator2(request.POST)
+    if len(errors1)>0:
+        for key ,values in errors1.items():
+            messages.error(request,values)
+        return redirect('/shows/' + str(num2) + '/edit')
+    else:
+        up=Show.objects.get(id=int(num2))
+        up.title=request.POST['ttname']
+        up.network=request.POST['nett']
+        up.description=request.POST['descr']
+        up.releasedate=request.POST['releasedatee']
+        up.save()
+        messages.success(request, "Show successfully updated")
+        return redirect('/shows/' + str(num2))
     
-
-    return render(request,'form.html')
-
-def handle(request):
-       
-        request.session['number']=int(request.POST['guess'])
-        num1=request.session['number'] 
-
-        if num1 < request.session['num']:
-            
-            return redirect('/low')
-        elif num1==request.session['num']:
-            return redirect('/correct')
-        
-        else:
-            
-            return redirect('/high')
+def method7(request,num1):
     
-def low(request):
-    return render(request,'form.html')
-def high(request):
-    return render(request,'form.html')
-def correct(request):
-    return render(request,'correct.html')
-
-
-
-
-
-
-
-    
-
-# Create your views here.
+    context={
+         'edit':Show.objects.get(id=int(num1))
+     }
+    return render(request ,'update.html' ,context)
