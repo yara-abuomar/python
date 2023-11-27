4c50e800f83361b0c840124e6539d1813903fb70
yara-abuomar <147878514+yara-abuomar@users.noreply.github.com>
2023-11-28 01:19:08 +0200

    Amadon

--- app/models.py
+++ app/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-class User(models.Model):
-    first_name=models.CharField(max_length=255)
-    last_name=models.CharField(max_length=255)
-    email=models.CharField(max_length=255)
-    age=models.IntegerField()
-    create_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now=True)
 
+class Product(models.Model):
+    item=models.CharField(max_length=255)
+    price=models.FloatField()
+   # maxquntity=models.IntegerField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
 
 # Create your models here.
